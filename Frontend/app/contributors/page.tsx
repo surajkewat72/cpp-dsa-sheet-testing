9cfe3a039ed4b38959a75d393e8d6117e1e08c01
Siddhika Savant <siddhikasavant00@gmail.com>
2025-07-26 21:19:40 +0530

    feat: add contributors page

---
+++ Frontend/app/contributors/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ContributorsPage = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold mb-4">✨ Our Amazing Contributors</h1>
+      <p className="text-gray-600">This page will show dynamic contributor stats soon!</p>
+    </div>
+  );
+};
+
+export default ContributorsPage;
