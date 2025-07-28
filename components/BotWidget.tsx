989301e4786ac9663c7fdacbdc638bf02342a941
sourashmi23 <138985338+sourashmi23@users.noreply.github.com>
2025-07-28 13:58:31 +0530

    Update BotWidget.tsx

--- components/BotWidget.tsx
+++ components/BotWidget.tsx
@@ -42,7 +42,7 @@ export default function BotWidget() {
   };
 
   return (
-    <div className="fixed bottom-6 right-6 z-50">
+    <div className="fixed bottom-20 right-6 z-50">
       <button
         onClick={() => setOpen(!open)}
         className="bg-blue-600 w-16 h-16 rounded-full shadow-lg flex items-center justify-center"
