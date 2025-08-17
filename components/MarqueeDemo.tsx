5b82721d4310471453e05472fb679740369dbb6b
gunjanghate <ghategunjan@gmail.com>
2025-08-17 21:01:25 +0530

    Updated testimonial section as approved-1

--- components/MarqueeDemo.tsx
+++ components/MarqueeDemo.tsx
@@ -116,7 +116,7 @@ const ReviewCard = ({
           "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
         )}
       >
-        <div className="flex flex-row items-center gap-2">
+        <div className="flex flex-row items-start gap-2">
           <img className="rounded-full" width="32" height="32" alt="" src={img} />
           <div className="flex flex-col ">
             <figcaption className="text-sm font-medium dark:text-white">
