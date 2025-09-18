84b8e8d66cdb2307d987d45e18df6917afa27215
gunjanghate <ghategunjan@gmail.com>
2025-09-18 20:25:20 +0530

    feat: Added Post Route for questions

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,5 @@
 import { Brain, Star, Zap } from "lucide-react";
 import Image from "next/image";
-import banner from "../../public/assets/banner.png";
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col md:flex-row min-h-screen bg-black  animate-in fade-in slide-in-from-bottom-6">
@@ -9,7 +8,7 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
       <div
         className="hidden md:flex md:flex-col md:w-1/2 text-white p-10 justify-center space-y-6 relative transition-all duration-300 rounded-r-2xl h-screen "
         style={{
-          backgroundImage: `url(${banner.src})`,
+          backgroundImage: `url(/assets/banner.png)`,
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
@@ -17,29 +16,29 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
       >
         <div className=" bg-transparent backdrop-blur-2xl border-2 rounded-3xl flex flex-col justify-center items-center py-6">
 
-        <div className="flex flex-col items-center text-center space-y-2">
-          <Image
-        src={"/icons/icon-192.png"}
-        alt="DSAMate Logo"
-        width={150}
-        height={130}
-        className="mb-10  shake-hover"
-        />
-          <h1 className="text-2xl lg:text-3xl font-bold leading-tight">Make it to DSA</h1>
-          <p className="text-base text-white/90 max-w-sm">Get all DSA problems here to boost your career.</p>
-        </div>
+          <div className="flex flex-col items-center text-center space-y-2">
+            <Image
+              src={"/icons/icon-192.png"}
+              alt="DSAMate Logo"
+              width={150}
+              height={130}
+              className="mb-10  shake-hover"
+            />
+            <h1 className="text-2xl lg:text-3xl font-bold leading-tight">Make it to DSA</h1>
+            <p className="text-base text-white/90 max-w-sm">Get all DSA problems here to boost your career.</p>
+          </div>
 
-        <div className="space-y-6 mt-10">
-          <FeatureItem icon={<Zap className="text-black" />} title="Smooth User Expirence" desc="Find your problem to get started with." />
-          <FeatureItem icon={<Brain className="text-black" />} title="Question Index" desc="All questions in one place." />
-          <FeatureItem icon={<Star className="text-black" />} title="Smart Insights" desc="AI helps to make your progress better." />
-        </div>
+          <div className="space-y-6 mt-10">
+            <FeatureItem icon={<Zap className="text-black" />} title="Smooth User Expirence" desc="Find your problem to get started with." />
+            <FeatureItem icon={<Brain className="text-black" />} title="Question Index" desc="All questions in one place." />
+            <FeatureItem icon={<Star className="text-black" />} title="Smart Insights" desc="AI helps to make your progress better." />
+          </div>
         </div>
       </div>
 
       {/* Right Side: Auth Form */}
       <div className="flex flex-col w-full md:w-1/2 justify-center items-center p-6 sm:p-8 md:h-screen">
-        <Image src={"/icons/icon-192.png"} alt="DSAMate logo" width={100} height={100} className="block md:hidden m-5 w-[100px] h-[100px]"/>
+        <Image src={"/icons/icon-192.png"} alt="DSAMate logo" width={100} height={100} className="block md:hidden m-5 w-[100px] h-[100px]" />
         <div className="w-full max-w-md">
           {children}
         </div>
